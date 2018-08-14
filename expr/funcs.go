f9afda9c3f9585715a2c81e0cf8eed714c20acc9
Stiven Deleur <sdeleur@bloomberg.net>
2018-08-14 11:49:25 -0400

    make function stable

--- expr/funcs.go
+++ expr/funcs.go
@@ -69,7 +69,7 @@ func init() {
 		"highestCurrent":    {NewHighestLowestConstructor("current", true), true},
 		"highestMax":        {NewHighestLowestConstructor("max", true), true},
 		"isNonNull":         {NewIsNonNull, true},
-		"keepLastValue":     {NewKeepLastValue, false},
+		"keepLastValue":     {NewKeepLastValue, true},
 		"lowest":            {NewHighestLowestConstructor("", false), true},
 		"lowestAverage":     {NewHighestLowestConstructor("average", false), true},
 		"lowestCurrent":     {NewHighestLowestConstructor("current", false), true},
